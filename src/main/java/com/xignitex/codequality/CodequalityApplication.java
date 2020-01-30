6df30a22d50cd13dac796be6c3733cdfd8781918
Oskar Albers <oalbers@wundermancommerce.com>
2020-01-30 09:38:45 +0100

    first commit

---
+++ src/main/java/com/xignitex/codequality/CodequalityApplication.java
@@ -0,0 +1,13 @@
+package com.xignitex.codequality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodequalityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodequalityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xignitex/codequality/CodequalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xignitex.codequality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodequalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
